8a90d0d56ec0fa317f69815394a1f9ff55f6e587
James Darling <jd@berglondon.com>
2013-02-15 16:46:54 +0000

    initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Courtfinder::Application.config.secret_token = '3acc8e27844801e1300d68710488050ea3e8de4802e5f75939b73292ac7ce740c789fb403a0157cd5323e3e67a6ab46bbe9cacbb5d5f1c18c2e3773c2ea25f67'
