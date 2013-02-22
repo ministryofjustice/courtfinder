5d7e55d16c246f5802ef2e91efacde2d101178a9
Mat Harden <matharden@gmail.com>
2013-02-22 12:19:54 +0000

    Re-enabled court API
    Enabled custom number of results in court results

--- app/controllers/courts_controller.rb
+++ app/controllers/courts_controller.rb
@@ -1,22 +1,24 @@
 class CourtsController < ApplicationController
   
+  respond_to :html, :json
+  
   def index 
     @courts = Court.all
     
     if params[:search]
-      @courts = Court.search(params[:search], params[:page], 15)
+      @courts = Court.search(params[:search], params[:page], params[:per_page] || 15)
     end
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @courts.to_json(:only => [:slug, :name]) }
-    end
+    respond_with @courts
   end
   
   def show
     @court = Court.find(params[:id])
-    if request.path != court_path(@court)
-      redirect_to @court, status: :moved_permanently
+
+    if request.path != court_path(@court, :format => params[:format])
+      redirect_to court_path(@court, :format => params[:format]), status: :moved_permanently
+    else
+      respond_with @court
     end
   end
 
