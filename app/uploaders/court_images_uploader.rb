a91aad466797002e903f22890b08ad43d7cdaff0
Mat Harden <matharden@gmail.com>
2013-04-24 15:14:28 +0100

    Sourced photos from existing court finder

--- app/uploaders/court_images_uploader.rb
+++ app/uploaders/court_images_uploader.rb
@@ -17,7 +17,7 @@ class CourtImagesUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -48,8 +48,8 @@ class CourtImagesUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    "#{model.name.parameterize.underscore}.#{model.image.split('.').last}" if original_filename
+  end
 
 end
