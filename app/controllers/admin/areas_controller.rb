0989a0483d1dee9ba04eec6740a88ccae75a827f
Mat Harden <matharden@gmail.com>
2013-05-08 17:15:55 +0100

    Tidy of Areas admin

--- app/controllers/admin/areas_controller.rb
+++ app/controllers/admin/areas_controller.rb
@@ -46,7 +46,7 @@ class Admin::AreasController < ApplicationController
 
     respond_to do |format|
       if @area.save
-        format.html { redirect_to admin_area_path(@area), notice: 'Area was successfully created.' }
+        format.html { redirect_to edit_admin_area_path(@area), notice: 'Area was successfully created.' }
         format.json { render json: @area, status: :created, location: @area }
       else
         format.html { render action: "new" }
@@ -62,7 +62,7 @@ class Admin::AreasController < ApplicationController
 
     respond_to do |format|
       if @area.update_attributes(params[:area])
-        format.html { redirect_to admin_area_path(@area), notice: 'Area was successfully updated.' }
+        format.html { redirect_to edit_admin_area_path(@area), notice: 'Area was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
