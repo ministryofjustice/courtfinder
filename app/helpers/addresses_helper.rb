fe7188b10864bfe22f7d9a9172e95108e99c7ad7
Max Froumentin <max@lapin-bleu.net>
2013-05-23 14:07:19 +0100

    new helper for addresses, using HTML with schema.org attributes

--- app/helpers/addresses_helper.rb
+++ app/helpers/addresses_helper.rb
@@ -1,2 +1,11 @@
 module AddressesHelper
+  def format_address(addr)
+    ("<p itemscope itemtype='http://schema.org/PostalAddress' itemprop='address'>
+       <span itemprop='streetAddress'>#{addr.addressLines('<br />')}</span><br/>" +
+      (addr.town.name ? "<span itemprop='addressLocality'>#{addr.town.name}</span><br/>" : '') +
+       "<span itemprop='county'>#{addr.town.county.name}</span><br/>
+       <span itemprop='postalCode'>#{addr.postcode}</span><br/>
+       #{addr.dx}
+     </p>").html_safe
+  end
 end
