b7f57f856b181fe08abf58c32d05739243b63ff1
Max Froumentin <max@lapin-bleu.net>
2013-05-29 09:36:12 +0100

    metadata fixes

--- app/helpers/addresses_helper.rb
+++ app/helpers/addresses_helper.rb
@@ -1,9 +1,9 @@
 module AddressesHelper
   def format_address(addr)
-    ("<p itemscope itemtype='http://schema.org/PostalAddress' itemprop='address'>
+    ("<p>
        <span itemprop='streetAddress'>#{addr.addressLines('<br />')}</span><br/>" +
       (addr.town.name ? "<span itemprop='addressLocality'>#{addr.town.name}</span><br/>" : '') +
-       "<span itemprop='county'>#{addr.town.county.name}</span><br/>
+       "<span itemprop='addressRegion'>#{addr.town.county.name}</span><br/>
        <span itemprop='postalCode'>#{addr.postcode}</span><br/>
        #{addr.dx}
      </p>").html_safe
