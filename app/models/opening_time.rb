b9eacb81328bc6dd915972dd7339b785736cf4e2
Mat Harden <matharden@gmail.com>
2013-06-14 12:33:03 +0100

    Fix for opening times order

--- app/models/opening_time.rb
+++ app/models/opening_time.rb
@@ -2,4 +2,6 @@ class OpeningTime < ActiveRecord::Base
   belongs_to :court
   belongs_to :opening_type
   attr_accessible :name, :sort, :court_id, :opening_type_id
+
+  default_scope :order => :id
 end
