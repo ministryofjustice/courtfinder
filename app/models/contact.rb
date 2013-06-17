49bcf93e2d3b2aa4cd7409338161143969ba09bc
Mat Harden <matharden@gmail.com>
2013-06-18 00:02:23 +0100

    Contact and Facility sorting fixes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,6 @@ class Contact < ActiveRecord::Base
   belongs_to :court
   belongs_to :contact_type
   attr_accessible :in_leaflet, :name, :sort, :telephone, :contact_type_id
+
+  default_scope :order => :sort
 end
--- app/models/court_facility.rb
+++ app/models/court_facility.rb
@@ -2,4 +2,6 @@ class CourtFacility < ActiveRecord::Base
   belongs_to :court
   belongs_to :facility
   attr_accessible :description, :facility_id, :sort
+
+  default_scope :order => :sort
 end
