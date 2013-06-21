319d0470d19034921aa2a776ae73e807b4867b19
Max Froumentin <max@lapin-bleu.net>
2013-06-21 12:19:11 +0100

    changed to RDFa and made a courthouse both a CourtHouse and a GovernmentOrganization

--- app/helpers/addresses_helper.rb
+++ app/helpers/addresses_helper.rb
@@ -1,10 +1,10 @@
 module AddressesHelper
   def format_address(addr)
-    ("<p itemscope itemtype='http://schema.org/PostalAddress' itemprop='address'>
-       <span itemprop='streetAddress'>#{addr.address_lines('<br />')}</span><br/>" +
-      (addr.town.name ? "<span itemprop='addressLocality'>#{addr.town.name}</span><br/>" : '') +
-       "<span itemprop='addressRegion'>#{addr.town.county.name}</span><br/>
-       <span itemprop='postalCode'>#{addr.postcode}</span><br/>
+    ("<p typeof='PostalAddress' property='address'>
+       <span property='streetAddress'>#{addr.address_lines('<br />')}</span><br/>" +
+      (addr.town.name ? "<span property='addressLocality'>#{addr.town.name}</span><br/>" : '') +
+       "<span property='addressRegion'>#{addr.town.county.name}</span><br/>
+       <span property='postalCode'>#{addr.postcode}</span><br/>
        #{addr.dx}
      </p>").html_safe
   end
