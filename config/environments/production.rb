b0d3088710fe4f5b9461b787efc08a5895371472
Jan Szumiec <jan.szumiec@gmail.com>
2013-07-12 15:56:06 +0100

    Changed port to 587 and enabled STARTTLS.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,10 +73,11 @@ Courtfinder::Application.configure do
 
   config.action_mailer.smtp_settings = {
     address: ENV['SMTP_HOSTNAME'],
-    port: 465,
+    port: 587,
     domain: ENV['SMTP_DOMAIN'],
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
-    authentication: :login
+    authentication: :login,
+    enable_starttls_auto: true
   }
 end
