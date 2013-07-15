449f4051061a33a2d717371dd1f61105e9f86ac6
Jan Szumiec <jan.szumiec@gmail.com>
2013-07-15 16:14:57 +0100

    Session token passthrough.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Courtfinder::Application.config.secret_token = '3acc8e27844801e1300d68710488050ea3e8de4802e5f75939b73292ac7ce740c789fb403a0157cd5323e3e67a6ab46bbe9cacbb5d5f1c18c2e3773c2ea25f67'
+Courtfinder::Application.config.secret_token = Rails.env.production? ? ENV["SESSION_SECRET_KEY"] : "3c1c638b115edba065da04e99e236eae"
+
