57da1f50df41587ce46f9b84f1c9516d769e2048
Mat Harden <matharden@gmail.com>
2013-07-16 12:23:12 +0100

    Removed line break when county is blank
    Fixes: https://trello.com/c/d1oVZtx6/128-london-does-not-appear-as-a-town

--- app/helpers/addresses_helper.rb
+++ app/helpers/addresses_helper.rb
@@ -1,11 +1,11 @@
 module AddressesHelper
   def format_address(addr)
-    ("<p property='address' typeof='http://schema.org/PostalAddress'>
-       <span property='streetAddress'>#{addr.address_lines('<br />')}</span><br/>" +
-      (addr.town.name ? "<span property='addressLocality'>#{addr.town.name}</span><br/>" : '') +
-       "<span property='addressRegion'>#{addr.town.county.name}</span><br/>
-       <span property='postalCode'>#{addr.postcode}</span><br/>
-       #{addr.dx}
-     </p>").html_safe
+    add = "<p property='address' typeof='http://schema.org/PostalAddress'>"
+    add << "<span property='streetAddress'>#{addr.address_lines('<br />')}</span><br/>"
+    add << "<span property='addressLocality'>#{addr.town.name}</span><br/>" if addr.town.name.present?
+    add << "<span property='addressRegion'>#{addr.town.county.name}</span><br/>" if addr.town.county.name.present?
+    add << "<span property='postalCode'>#{addr.postcode}</span><br/>#{addr.dx}</p>"
+
+    add.html_safe
   end
 end
