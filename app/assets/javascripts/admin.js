7997658c524c4acdc7853447a187f85efc637c44
Jan Szumiec <jan.szumiec@gmail.com>
2013-07-23 11:19:44 +0100

    CF-11 Removed a local copy of jquery and jquery-ui. Both are served off a CDN.       Added admin.js/css to a list of auxiliary precompiled assets.       jquery-migrate is now served locally.       Removed a duplicate: admin.scss.       Converted tabs to spaces (grrrr...)

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -10,11 +10,5 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require jquery.ui.core
-//= require jquery.ui.widget
-//= require jquery.ui.mouse
-//= require jquery.ui.sortable
 //= require ckeditor-jquery
 //= require admin/courts
