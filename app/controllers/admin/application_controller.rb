d8ac8603cfe0de22dbbdf818026821dbd5dcec77
Jan Szumiec <jan.szumiec@gmail.com>
2013-08-14 12:10:45 +0100

    Quick fix: enabled cache control.

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,4 +2,5 @@ class Admin::ApplicationController < ::ApplicationController
   protect_from_forgery
 
   before_filter :authenticate_user!
+  skip_before_filter :set_expires_to_3_hours
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
   WillPaginate.per_page = 50
 
+  before_filter :set_expires_to_3_hours
+
   def after_sign_in_path_for(resource)
     admin_path
   end
 
   def after_sign_out_path_for(resource)
     new_user_session_path
+  end
+
+  def set_expires_to_3_hours
+    expires_in(3.hours, public: true)
   end
 end
