9c37efa1075d336f1ab86d49e7081aa1f9318b43
Jan Szumiec <jan.szumiec@gmail.com>
2013-08-15 11:20:14 +0100

    CF-76 Application-level redirects for legacy URLs.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,17 @@
 class HomeController < ApplicationController
   def index
+    if leaflet_id = params[:court_leaflets_id]
+      redirect_to("http://hmctsformfinder.justice.gov.uk/HMCTS/GetLeaflet.do?court_leaflets_id=#{leaflet_id}")
+    end
+
+    if form_id = params[:court_forms_id]
+      redirect_to("http://hmctsformfinder.justice.gov.uk/HMCTS/GetForm.do?court_forms_id=#{form_id}")
+    end
+
+    if court_id = params[:court_id]
+      redirect_to(court_path(Court.find_by_old_id(court_id)))
+    end
+
     @courts = Court.order(:name)
     # @court_types = CourtType.order(:name)
     @areas_of_law = AreaOfLaw.has_courts.order('areas_of_law.name')
---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe HomeController do
+  context "legacy url redirection" do
+    it "redirects by court_id" do
+      court = Court.create!(old_id: 1, name: "A court of L.A.W.")
+      get :index, court_id: court.old_id
+      response.should redirect_to(court_path(court))
+    end
+
+    it "redirects to a legacy formfinder leaflet" do
+      get :index, court_leaflets_id: 0
+      response.should redirect_to("http://hmctsformfinder.justice.gov.uk/HMCTS/GetLeaflet.do?court_leaflets_id=0")
+    end
+
+    it "redirects to a legacy formfinder form" do
+      get :index, court_forms_id: 0
+      response.should redirect_to("http://hmctsformfinder.justice.gov.uk/HMCTS/GetForm.do?court_forms_id=0")
+    end
+  end
+end
