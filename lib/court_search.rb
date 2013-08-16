815a3273ef720e8699992513c2f0a1360785f9d7
Jan Szumiec <jan.szumiec@gmail.com>
2013-08-16 15:44:04 +0100

    Tiny cleanup of CourtsSearch.

--- lib/court_search.rb
+++ lib/court_search.rb
@@ -19,35 +19,25 @@ class CourtSearch
       return
     else
       if postcode_search?
-        search_postcode
+        if latlng = latlng_from_postcode(@query)
+          Court.visible.by_area_of_law(@options[:area_of_law]).near(latlng, @options[:distance] || 20)
+        else
+          @errors << "We couldn't find that post code. Please try again."
+          return
+        end
       else
         Court.visible.by_area_of_law(@options[:area_of_law]).search(@query)
       end
     end
   end
 
-  def search_postcode
-    # Use the geocoder near method to find venues within the specified radius
-    latlon = latlng_from_postcode(@query)
-    # if latlon
-      Court.visible.by_area_of_law(@options[:area_of_law]).near(latlon, @options[:distance] || 20)
-    # else
-      # @errors.push 'This is an error'
-      # return nil
-    # end
-  end
-
   def latlng_from_postcode(postcode)
     # Use PHP postcode service to turn postcode into lat/lon
-    service_available = Rails.application.config.postcode_lookup_service_url rescue false
-    json = @restclient.get(:params => { :searchtext => postcode, :searchbtn => 'Search' })
-    results = JSON.parse json
-    [results['primary']['coordinates']['lat'], results['primary']['coordinates']['long']]
+    results = JSON.parse(@restclient.get(:params => { :searchtext => postcode, :searchbtn => 'Search' }))
+    [results['primary']['coordinates']['lat'], results['primary']['coordinates']['long']] unless results['error']
   end
 
   def postcode_search?
-    # Allow full postcode (e.g. W4 1SE) only
-    # @query =~ /^([g][i][r][0][a][a])$|^((([a-pr-uwyz]{1}\d{1,2})|([a-pr-uwyz]{1}[a-hk-y]{1}\d{1,2})|([a-pr-uwyz]{1}\d{1}[a-hjkps-uw]{1})|([a-pr-uwyz]{1}[a-hk-y]{1}\d{1}[a-z]{1}))\d[abd-hjlnp-uw-z]{2})$/i
     # Allow full postcode (e.g. W4 1SE) or outgoing postcode (e.g. W4)
     @query =~ /^([g][i][r][0][a][a])$|^((([a-pr-uwyz]{1}\d{1,2})|([a-pr-uwyz]{1}[a-hk-y]{1}\d{1,2})|([a-pr-uwyz]{1}\d{1}[a-hjkps-uw]{1})|([a-pr-uwyz]{1}[a-hk-y]{1}\d{1}[a-z]{1})) ?(\d[abd-hjlnp-uw-z]{2})?)$/i
   end
--- spec/lib/courts_search_spec.rb
+++ spec/lib/courts_search_spec.rb
@@ -11,13 +11,11 @@ describe CourtSearch do
 
   it "should return courts nearby if full postcode search" do
     court_search = CourtSearch.new('hp41du')
-    # court_search.stub!(:latlng_from_postcode).and_return([51.768305511577, -0.57250059493886])
     court_search.results.should == [@court1]
   end
 
   it "should return courts nearby if partial postcode" do
     court_search = CourtSearch.new('hp4')
-    # court_search.stub!(:latlng_from_postcode).and_return([51.768305511577, -0.57250059493886])
     court_search.results.should == [@court1]
   end
 
@@ -30,16 +28,6 @@ describe CourtSearch do
   it "should return courts by name if search is not a postcode" do
     CourtSearch.new('London').results.should == [@court2]
   end
-
-  # it "should return courts by name and court type if both match" do
-  #   @court_type = FactoryGirl.create(:court_type, :name => 'London courts')
-  #   CourtSearch.new('London').results.should == {:courts => [@court2], :court_types => [@court_type], :areas_of_law => []}
-  # end
-
-  # it "should return courts by name and areas of law if both match" do
-  #   @area_of_law = FactoryGirl.create(:area_of_law, :name => 'London crime')
-  #   CourtSearch.new('London').results.should == {:courts => [@court2], :court_types => [], :areas_of_law => [@area_of_law]}
-  # end
 
   it "should limit results to area of law when specified" do
     # should return court 3 and not court 4
