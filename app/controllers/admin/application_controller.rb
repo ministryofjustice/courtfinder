7d66d4c7a34fd40bec3e91c6b8e9426f2826e71b
Jan Szumiec <jan.szumiec@gmail.com>
2013-09-05 12:47:24 +0100

    Fix for varnish cache expiration.

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -3,19 +3,13 @@ class Admin::ApplicationController < ::ApplicationController
 
   before_filter :authenticate_user!
 
-  def purge_cache(string_or_regex)
+  def purge_cache(regex_as_string)
     unless Rails.env.development?
-      client = Varnish::Client.new('127.0.0.1', 80, request.host)
-      case string_or_regex
-      when String
-        client.ban(string_or_regex)
-      when Regexp
-        client.purge(string_or_regex.to_s)
-      end
+      Varnish::Client.new('127.0.0.1', 80, ['http://', request.host].join).purge(regex_as_string)
     end
   end
 
   def purge_all_pages
-    purge_cache(/.*/)
+    purge_cache('.*')
   end
 end
--- spec/controllers/admin_application_controller_spec.rb
+++ spec/controllers/admin_application_controller_spec.rb
@@ -5,18 +5,13 @@ describe Admin::ApplicationController do
     setup_controller_request_and_response
   end
 
-  it "tells varnish to purge its cache based on a location" do
-    Varnish::Client.any_instance.should_receive(:ban).with(location = '/some.location')
-    controller.purge_cache(location)
-  end
-
   it "tells varnish to purge its cache based on a regexp" do
-    Varnish::Client.any_instance.should_receive(:purge).with(regex = /^.*$/)
+    Varnish::Client.any_instance.should_receive(:purge).with(regex = '.*')
     controller.purge_cache(regex)
   end
 
   it "tells varnish to purge all pages" do
-    controller.should_receive(:purge_cache).with(/.*/)
+    controller.should_receive(:purge_cache).with('.*')
     controller.purge_all_pages
   end
 end
