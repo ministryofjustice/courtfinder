a05f6e6673c958e8d72feefc49d334ac4ec41924
Hrishi Mittal <hrishimittal@gmail.com>
2013-09-06 13:20:11 +0100

    CF115 add optional email field to feedback form
    Add more test specs for feedback form

--- spec/mailers/notifications_mailer_spec.rb
+++ spec/mailers/notifications_mailer_spec.rb
@@ -3,19 +3,40 @@ require "spec_helper"
 describe NotificationsMailer do
   before :each do
     NotificationsMailer.default to: "test@dsd.io"
-  end
-
-  it "composes mail that contains user-submitted feedback form values" do
-    form_params = {
+    @form_params = {
         :feedback => {
             "rating" => 5,
-            "text" => "what a beautiful site"
+            "text" => "what a beautiful site",
+            "email" => "hermajesty@imperialawesomeness.com"
             },
         "browser" => "Chrome",
         "service" => "Court finder",
         "referrer" => "some-page-on-the-site"
     }
-    email = NotificationsMailer.new_message(form_params)
-    email.body.raw_source.should include(form_params[:feedback]["text"])
+    @feedback_email = NotificationsMailer.new_message(@form_params)
+  end
+
+  it "feedback notification email contains user-submitted feedback rating" do
+    @feedback_email.body.raw_source.should include(@form_params[:feedback]["rating"].to_s)
+  end
+
+  it "feedback notification email contains user-submitted feedback text" do
+    @feedback_email.body.raw_source.should include(@form_params[:feedback]["text"])
+  end
+
+  it "feedback notification email contains user-submitted feedback email" do
+    @feedback_email.body.raw_source.should include(@form_params[:feedback]["email"])
+  end
+
+  it "feedback notification email contains user browser" do
+    @feedback_email.body.raw_source.should include(@form_params["browser"])
+  end
+
+  it "feedback notification email contains service name" do
+    @feedback_email.body.raw_source.should include(@form_params["service"])
+  end
+
+  it "feedback notification email contains referrer" do
+    @feedback_email.body.raw_source.should include(@form_params["referrer"])
   end
 end
