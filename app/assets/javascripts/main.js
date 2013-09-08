3c2254779ffcc0521075e528aabe482bf5efdb1e
Mat Harden <matharden@gmail.com>
2013-09-08 22:57:31 +0100

    Move admin JavaScript into admin only file

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,42 +1,12 @@
 /*jslint browser: true, evil: false, plusplus: true, white: true, indent: 2 */
 /*global moj, $ */
 
-// Initialise all moj modules
-moj.init();
-
 (function(){
 
   "use strict";
 
-  // Admin
-  // ==================================================================
-
-  // Adding/removing records
-  $('form').on('click', '.remove_fields', function (e) {
-    e.preventDefault();
-    $(this).prev('input[type=hidden]').val('1');
-    $(this).closest('fieldset').hide();
-  });
-
-  $('form').on('click', '.add_fields', function (e) {
-    e.preventDefault();
-    var time = new Date().getTime(),
-      regexp = new RegExp($(this).data('id'), 'g'),
-      callback = $(this).data('callback'),
-      sortable = $(this).siblings('ul.sortable'),
-      fields = $($(this).data('fields').replace(regexp, time)),
-      obj;
-    
-    if (sortable.length) {
-      obj = sortable.append(fields);
-    } else {
-      obj = $(this).before(fields);
-    }
-
-    if (callback) {
-      moj[callback](obj, fields);
-    }
-  });
+  // Initialise all moj modules
+  moj.init();
 
   // Open browser print dialog
   $('.print-link').on('click', function (e) {
