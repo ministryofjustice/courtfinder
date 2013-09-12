78d88708224904cf619f9ea62631814b958935d2
Jan Szumiec <jan.szumiec@gmail.com>
2013-09-12 15:30:32 +0100

    CF-101 Cookies are set only on the /admin path.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Courtfinder::Application.config.session_store :cookie_store, key: '_courtfinder_session'
+Courtfinder::Application.config.session_store :cookie_store, key: 'CFSID', path: '/admin', httponly: true
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
---
+++ spec/configuration/session_cookie_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Courtfinder::Application.config do
+  it "sets cookie only on the /admin path" do
+    subject.session_store.should == ActionDispatch::Session::CookieStore
+    subject.session_options.slice(:cookie_only, :key, :path, :httponly).should == {cookie_only: true, key: 'CFSID', path: '/admin', httponly: true}
+  end
+end
