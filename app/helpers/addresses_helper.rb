82b9c3610ec98ab1f4dc3d2c97e1434a8dfa5838
Jan Szumiec <jan.szumiec@gmail.com>
2013-11-13 16:14:53 +0000

    CF-200 When a town for an address is blank, don't blow up.

--- app/helpers/addresses_helper.rb
+++ app/helpers/addresses_helper.rb
@@ -2,8 +2,10 @@ module AddressesHelper
   def format_address(addr)
     add = "<p property='address' typeof='http://schema.org/PostalAddress'>"
     add << "<span property='streetAddress'>#{addr.address_lines('<br />')}</span><br/>"
-    add << "<span property='addressLocality'>#{addr.town.name}</span><br/>" if addr.town.name.present?
-    add << "<span property='addressRegion'>#{addr.town.county.name}</span><br/>" if (addr.town.county.present? && addr.town.county.name.present?)
+    if addr.town
+      add << "<span property='addressLocality'>#{addr.town.name}</span><br/>" if addr.town.name.present?
+      add << "<span property='addressRegion'>#{addr.town.county.name}</span><br/>" if (addr.town.county.present? && addr.town.county.name.present?)
+    end
     add << "<span property='postalCode'>#{addr.postcode}</span><br/>#{addr.dx}</p>" if addr.postcode.present?
 
     add.html_safe
