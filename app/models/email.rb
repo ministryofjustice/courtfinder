a68094c4e4a50654e3f071c669826bae4226a188
Mat Harden <matharden@gmail.com>
2014-02-05 15:29:07 +0000

    Added email validator

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,9 +1,12 @@
+require 'email_validator'
+
 class Email < ActiveRecord::Base
   belongs_to :court
   belongs_to :contact_type
   attr_accessible :address, :sort, :contact_type_id
 
   has_paper_trail :ignore => [:created_at, :updated_at]
+  validates :address, email: true, if: ->(f) { f.address.present? }
 
   default_scope :order => :sort
 
