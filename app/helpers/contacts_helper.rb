065bb233b9865df364a77e6ea797ff361d2c2a0f
Mat Harden <matharden@gmail.com>
2014-02-13 15:13:18 +0000

    CF-224 refactored label logic and clearer names

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,28 +1,24 @@
 module ContactsHelper
-  def always_label(type, label='Other')
-    if type.nil?
-      return label
-    else
+  def enforce_label(type, label='Other')
+    if type
       name = ContactType.find(type).name
-      if name.blank?
-        return label
-      else
-        return name
-      end
+      name.blank? ? label : name
+    else
+      label
     end
   end
 
-  def contact_visible_in_this_view(contact)
+  def contact_visible_in_view(contact)
     (print_view && contact.in_leaflet) || !print_view
   end
 
-  def contacts_in_this_view(contacts)
+  def contacts_for_view(contacts)
     contacts.reject do |contact|
-      contact_visible_in_this_view(contact) == false
+      contact_visible_in_view(contact) == false
     end
   end
 
-  def display_as_group(contacts, glue=' or ')
+  def contacts_as_group(contacts, glue=' or ')
     contact_list = []
     
     contacts.each do |contact|
