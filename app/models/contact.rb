e5ee092f8dad331006e0151a7337ab9fecaa55d0
Hrishi Mittal <hrishimittal@gmail.com>
2014-02-21 17:07:19 +0000

    Don't allow duplicate phone numbers to be added to court contacts
    Fix phone validation specs
    
    Fix contact spec

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,13 +3,13 @@ class Contact < ActiveRecord::Base
   belongs_to :contact_type
   attr_accessible :in_leaflet, :name, :sort, :telephone, :contact_type_id
 
-  validates :telephone, presence:true
-
-  #Allow numbers and spaces only
-  validates :telephone, :format => { :with =>  /^(?=.*\d)(?:[\d ]+)$/, 
-                                    :message => "is invalid. Please enter a phone number, only digits and spaces allowed."}
+  validates :telephone, presence:true, contact: true
 
   has_paper_trail :ignore => [:created_at, :updated_at]
 
   default_scope :order => :sort
+
+  def contact_type_name
+    self.contact_type.name.blank? ? "another service" : self.contact_type.name
+  end
 end
