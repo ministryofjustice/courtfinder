c2ae750f0ef2c23af55f867eab49fd4e3ce64053
Hrishi Mittal <hrishimittal@gmail.com>
2014-02-21 17:07:19 +0000

    Don't allow duplicate phone numbers to be added to court contacts
    Fix phone validation specs

---
+++ lib/contact_validator.rb
@@ -0,0 +1,26 @@
+class ContactValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    if check_phone_format(value).nil?
+      record.errors.add(attribute, "is invalid. Please enter a phone number, only digits and spaces allowed.") 
+    elsif service = check_duplicate_number(record, value)
+      record.errors.add(attribute, "is invalid. This number is already entered for #{service}.") 
+    end
+  end
+
+  private
+  def check_duplicate_number(contact, phone_number)
+    court_contacts = contact.court.contacts
+    if i = find_number_in_existing_contacts(court_contacts, phone_number)
+      court_contacts[i].contact_type_name
+    end
+  end
+
+  def find_number_in_existing_contacts(contacts, number)
+    #Strip whitespace of existing contact numbers and check if new number already exists
+    contacts.pluck(:telephone).collect { |t| t.gsub(" ","") }.index(number.gsub(" ",""))
+  end
+
+  def check_phone_format(phone_number)
+    phone_number =~ /^(?=.*\d)(?:[\d ]+)$/
+  end
+end
