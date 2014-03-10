846384c5e9f0340e78207bf7f36336e2307f6855
Jairo Diaz <jairo.diaz@codescrum.com>
2014-03-10 13:30:48 +0000

    Refactored tests to accept new area of law

--- spec/models/local_authority_spec.rb
+++ spec/models/local_authority_spec.rb
@@ -3,5 +3,5 @@ require "spec_helper"
 describe LocalAuthority do
   it { should belong_to :court }
   it { should belong_to :council }
-  it { should respond_to :area_of_law }
+  it { should respond_to :type }
 end
