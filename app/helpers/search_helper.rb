2da7f512a70e01085c3e44e3dea1376cb5f128c3
Danny Hawkins <danny.hawkins@gmail.com>
2014-03-17 17:54:16 +0000

    added search helper

---
+++ app/helpers/search_helper.rb
@@ -0,0 +1,7 @@
+module SearchHelper
+
+  def family_court_search_found(search, area_of_law)
+    "#{'Court'.pluralize(search.results.size)} dealing with applications involving #{area_of_law.name.downcase} for the post code <strong>#{search.query}</strong>".html_safe
+  end
+
+end
