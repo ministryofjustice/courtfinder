a8a985ddbcabe7981ab2b643799dd085b5b3fffe
Ikenna Okpala <ikenna.okpala@digital.justice.gov.uk>
2014-04-30 14:55:39 +0100

    define format method

--- lib/ip_location.rb
+++ lib/ip_location.rb
@@ -3,6 +3,18 @@ module IpLocation
     url = "http://freegeoip.net/json/#{ip}"
     attributes = ['country_name', 'region_name', 'city']
     location = JSON.parse(RestClient.get(url))
-    location.slice(*attributes).values.compact.join
+    format(location.slice(*attributes).values.compact)
   end
+
+  private
+
+    def format(details)
+      if details.count == 1 && == 'Reserved'
+        'localhost'
+      elsif details.count == 1
+        details.first
+      else
+        detsails.join('->')
+      end
+    end
 end
