3988e3440f5dca2f2e8cc6ce5557be138d9fab7f
Danny Hawkins <danny.hawkins@gmail.com>
2014-05-08 17:42:56 +0100

    updaed assets

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -5,10 +5,13 @@ module SearchHelper
   end
 
   def area_of_law_groups
-    grouped_data = AreaOfLawGroup.with_areas_of_law.includes(:areas_of_law).all
-    other = AreaOfLawGroup.new(name: 'Other')
-    other.areas_of_law = AreaOfLaw.where(group_id: nil).all
-    grouped_data << other
+    grouped_data = AreaOfLawGroup.with_areas_of_law.order('areas_of_law.name').includes(:areas_of_law).all
+    
+    if AreaOfLaw.where(group_id: nil).count
+      other = AreaOfLawGroup.new(name: 'Other')
+      other.areas_of_law = AreaOfLaw.where(group_id: nil).all
+      grouped_data << other
+    end
     grouped_data
   end
 
