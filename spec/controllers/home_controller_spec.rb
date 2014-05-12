f7c6c0523e11fb3736cd5d38c2b7a6d26acf1eee
Danny Hawkins <danny.hawkins@gmail.com>
2014-05-12 16:46:17 +0100

    using active record utc setup

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -8,7 +8,7 @@ describe HomeController do
   context "landing page" do
     it "displays the landing page" do
       controller.should_receive(:enable_varnish).twice
-      controller.should_receive(:fresh_when).with(last_modified: court.updated_at.utc, public: true).twice.and_call_original unless ENV['TRAVIS']
+      controller.should_receive(:fresh_when).with(last_modified: court.updated_at.utc, public: true).twice.and_call_original
       get :index
       response.should be_success
 
