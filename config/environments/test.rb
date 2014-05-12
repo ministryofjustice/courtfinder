6ced950d163ed781af4f3d3de0d20cf4190ba693
Danny Hawkins <danny.hawkins@gmail.com>
2014-05-12 16:42:59 +0100

    test time utc in test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -23,6 +23,7 @@ Courtfinder::Application.configure do
 
   # Disable request forgery protection in test environment
   config.action_controller.allow_forgery_protection    = false
+  config.active_record.default_timezone = :utc
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
