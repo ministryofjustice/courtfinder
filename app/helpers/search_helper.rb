809478d910e2b3c5c2d79675894b03261d302bb7
Danny Hawkins <danny.hawkins@gmail.com>
2014-05-13 16:48:56 +0100

    all added

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -6,13 +6,23 @@ module SearchHelper
 
   def area_of_law_groups
     grouped_data = AreaOfLawGroup.with_areas_of_law.order('areas_of_law.name').includes(:areas_of_law).all
-    
     if AreaOfLaw.where(group_id: nil).count
       other = AreaOfLawGroup.new(name: 'Other')
       other.areas_of_law = AreaOfLaw.where(group_id: nil).all
       grouped_data << other
     end
     grouped_data
+
+    html = content_tag(:option, 'All', value: 'all')
+    grouped_data.each do |group|
+      html << content_tag(:optgroup, label: group.name) do
+        group.areas_of_law.each do |area|
+          concat content_tag(:option, area.name, value: area.to_param)
+        end
+      end
+    end
+    
+    html
   end
 
 end
