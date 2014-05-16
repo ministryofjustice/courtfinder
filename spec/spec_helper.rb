07797d2ec44e1e409bae52ada803693d4f62cc0c
Danny Hawkins <danny.hawkins@gmail.com>
2014-05-16 14:01:45 +0100

    removed accept header

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -68,7 +68,7 @@ end
 require 'vcr'
 
 VCR.configure do |config|
-  config.default_cassette_options = { record: :new_episodes, serialize_with: :json }
+  config.default_cassette_options = { record: :none, serialize_with: :json }
   config.cassette_library_dir = 'spec/fixtures/cassettes'
   config.hook_into :webmock
   config.ignore_hosts '127.0.0.1' # allow selenium/capybara to do its thing
