1bfacdab24a92e5b9f7cf13085ed2d085ee741ae
Jairo Diaz <jairo.diaz@codescrum.com>
2014-06-06 09:44:20 +0100

    Backend implementation for related links

--- app/models/court_type.rb
+++ app/models/court_type.rb
@@ -2,6 +2,7 @@ class CourtType < ActiveRecord::Base
   attr_accessible :name, :old_id, :slug
   has_many :court_types_courts
   has_many :courts, :through => :court_types_courts
+  has_and_belongs_to_many :external_links
 
   has_paper_trail meta: {ip: :ip}
 
