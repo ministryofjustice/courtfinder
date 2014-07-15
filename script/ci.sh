e12734f7530db2883a5169bd30ef6ac350ed2cce
Jairo Diaz <jairo.diaz@codescrum.com>
2014-07-15 11:17:14 +0100

    Reordered env variables for executing ci

--- script/ci.sh
+++ script/ci.sh
@@ -4,5 +4,5 @@ RAILS_ENV=test bundle exec rake db:create
 RAILS_ENV=test bundle exec rake db:migrate
 RAILS_ENV=test bundle exec rake db:test:prepare
 # Configuration for Webkit on ci: http://stjhimy.com/posts/31-fixing-the-error-webkit_server-cannot-connect-to-x-server
-DISPLAY=localhost:1.0 xvfb-run RAILS_ENV=test bundle exec rspec spec
+DISPLAY=localhost:1.0 RAILS_ENV=test xvfb-run bundle exec rspec spec
 
