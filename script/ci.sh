8c6c1b0c53b4cb64244b558d16e7bdda07e1efd0
Jairo Diaz <jairo.diaz@codescrum.com>
2014-07-15 11:11:19 +0100

    Configured x-server for webkit on CI

--- script/ci.sh
+++ script/ci.sh
@@ -3,5 +3,6 @@ RAILS_ENV=test bundle install
 RAILS_ENV=test bundle exec rake db:create
 RAILS_ENV=test bundle exec rake db:migrate
 RAILS_ENV=test bundle exec rake db:test:prepare
-RAILS_ENV=test bundle exec rspec spec
+# Configuration for Webkit on ci: http://stjhimy.com/posts/31-fixing-the-error-webkit_server-cannot-connect-to-x-server
+DISPLAY=localhost:1.0 xvfb-run RAILS_ENV=test bundle exec rspec spec
 
