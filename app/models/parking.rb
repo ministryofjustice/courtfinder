cf5f9fc4f58173bbed4880186ab3d9399b700f20
Jairo Diaz <jairo.diaz@codescrum.com>
2014-07-16 15:53:26 +0100

    Added view to show parkings

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -1,9 +1,10 @@
 class Parking < ActiveRecord::Base
+  attr_accessible :parking_type, :paid
   belongs_to :court
 
-  INSIDE = :inside
-  OUTSIDE = :outside
+  #INSIDE = :inside
+  #OUTSIDE = :outside
 
-  validates_inclusion_of :parking_type, in: [INSIDE, OUTSIDE]
-  validates_presence_of :paid
+  #validates_inclusion_of :parking_type, in: [INSIDE, OUTSIDE]
+  #validates_presence_of :paid
 end
