eab872d50f788b3f502d822236b3ece7a0dd594f
Tom Stuart <tom@codon.com>
2014-10-24 17:02:55 +0100

    Expand hard tabs

--- spec/models/council_spec.rb
+++ spec/models/council_spec.rb
@@ -21,21 +21,21 @@ describe Council do
   end
 
   describe '#unassigned_for_area_of_law' do
-  	let(:children) { create(:area_of_law, name: 'Children') }
-  	let(:divorce) { create(:area_of_law, name: 'Divorce') }
-  	let(:council) { create(:council) }
-  	let(:court) { create(:court) }
+    let(:children) { create(:area_of_law, name: 'Children') }
+    let(:divorce) { create(:area_of_law, name: 'Divorce') }
+    let(:council) { create(:council) }
+    let(:court) { create(:court) }
 
-  	it 'returns a councils without assignments for area of law' do
-  		Council.unassigned_for_area_of_law(children).should eq([council])
-  	end
+    it 'returns a councils without assignments for area of law' do
+      Council.unassigned_for_area_of_law(children).should eq([council])
+    end
 
-  	it 'returns empty array when all all councils have assigned a court for an area of law' do
-  		council.court_council_links.create!(court: court, area_of_law: children)
-  		
-  		Council.unassigned_for_area_of_law(children).should eq([])
-  		Council.unassigned_for_area_of_law(divorce).should eq([council])
-  	end
+    it 'returns empty array when all all councils have assigned a court for an area of law' do
+      council.court_council_links.create!(court: court, area_of_law: children)
+      
+      Council.unassigned_for_area_of_law(children).should eq([])
+      Council.unassigned_for_area_of_law(divorce).should eq([council])
+    end
 
     context 'a court with councils assigned for multiple areas_of_law' do
       
