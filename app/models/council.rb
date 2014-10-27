c3994ed8f453e6bac1a4827fcd1f95ea39f7dbc3
Tom Stuart <tom@codon.com>
2014-10-27 10:38:38 +0000

    Clarify unassigned_for_area_of_law query
    Looking for a null council_id is weird, because we’re selecting a list
    of councils.

--- app/models/council.rb
+++ app/models/council.rb
@@ -20,8 +20,8 @@ class Council < ActiveRecord::Base
   scope :search, ->(query){ where('LOWER(name) like ?', "#{query.downcase}%").limit(10) }
 
   scope :unassigned_for_area_of_law, ->(area_of_law) {
-    joins("left outer join court_council_links on councils.id = court_council_links.council_id and court_council_links.area_of_law_id = #{area_of_law.respond_to?(:id) ? area_of_law.id : area_of_law}").
-    where('council_id is NULL')
+    joins("LEFT OUTER JOIN court_council_links ON councils.id = court_council_links.council_id AND court_council_links.area_of_law_id = #{area_of_law.respond_to?(:id) ? area_of_law.id : area_of_law}").
+    where('court_council_links.id IS NULL')
   }
 
 end
