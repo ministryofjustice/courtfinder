dfe3a1648929625f69e6f2c95d79622b7a514a31
Tom Stuart <tom@codon.com>
2014-10-27 13:59:57 +0000

    Find courts through jurisdictions
    This removes half of the work that CourtCouncilLink was doing.

--- spec/models/remit_spec.rb
+++ spec/models/remit_spec.rb
@@ -3,5 +3,6 @@ require 'spec_helper'
 describe Remit do
   describe 'associations' do
     it { is_expected.to have_many :jurisdictions }
+    it { is_expected.to have_many(:councils).through(:jurisdictions) }
   end
 end
