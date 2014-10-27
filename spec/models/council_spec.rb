349d4d7073468d8a86db52766d29ac2eeaea9fe7
Tom Stuart <tom@codon.com>
2014-10-27 15:50:48 +0000

    Make Council.unassigned_for_area_of_law tests more comprehensive
    This should provide more confidence that the scope is actually working
    as advertised.

--- spec/models/council_spec.rb
+++ spec/models/council_spec.rb
@@ -23,32 +23,44 @@ describe Council do
   describe '#unassigned_for_area_of_law' do
     include CourtCouncilHelper
 
-    let(:children) { create(:area_of_law, name: 'Children') }
-    let(:divorce) { create(:area_of_law, name: 'Divorce') }
-    let(:council) { create(:council) }
-    let(:court) { create(:court) }
+    let(:children) { create :area_of_law }
+    let(:divorce)  { create :area_of_law }
+
+    let(:children_court) { create :court }
+    let(:divorce_court)  { create :court }
+
+    before(:each) do
+      @council_with_both     = create :council
+      @council_with_children = create :council
+      @council_with_divorce  = create :council
+      @council_with_neither  = create :council
+
+      add_councils_to_court councils: [@council_with_both, @council_with_children], court: children_court, area_of_law: children
+      add_councils_to_court councils: [@council_with_both, @council_with_divorce], court: divorce_court, area_of_law: divorce
+    end
 
     it 'returns a councils without assignments for area of law' do
-      Council.unassigned_for_area_of_law(children).should eq([council])
+      unassigned_for_children = Council.unassigned_for_area_of_law(children)
+
+      expect(unassigned_for_children.size).to eq 2
+      expect(unassigned_for_children).to include @council_with_divorce, @council_with_neither
     end
 
     it 'returns empty array when all all councils have assigned a court for an area of law' do
-      add_councils_to_court councils: [council], court: court, area_of_law: children
-      
-      Council.unassigned_for_area_of_law(children).should eq([])
-      Council.unassigned_for_area_of_law(divorce).should eq([council])
+      add_councils_to_court councils: [@council_with_divorce, @council_with_neither], court: children_court, area_of_law: children
+
+      expect(Council.unassigned_for_area_of_law(children)).to be_empty
+      expect(Council.unassigned_for_area_of_law(divorce)).to include @council_with_children, @council_with_neither
     end
 
     context 'a court with councils assigned for multiple areas_of_law' do
-      
       it 'returns empty array when all all councils have assigned a court for an area of law' do
-        add_councils_to_court councils: [council], court: court, area_of_law: children
-        add_councils_to_court councils: [council], court: court, area_of_law: divorce
-        
-        Council.unassigned_for_area_of_law(children).should eq([])
+        add_councils_to_court councils: [@council_with_divorce, @council_with_neither], court: children_court, area_of_law: children
+        add_councils_to_court councils: [@council_with_children, @council_with_neither], court: divorce_court, area_of_law: divorce
+
+        expect(Council.unassigned_for_area_of_law(children)).to be_empty
+        expect(Council.unassigned_for_area_of_law(divorce)).to be_empty
       end
-
     end
-
   end
 end
