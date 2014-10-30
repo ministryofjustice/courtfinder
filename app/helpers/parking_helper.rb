e463aef9e2e8f222d4d462e93de750ee028780b8
Tom Stuart <tom@codon.com>
2014-10-30 15:26:33 +0000

    Add parking_onsite_none and parking_offsite_none and use them in (deprecated) front end

--- app/helpers/parking_helper.rb
+++ app/helpers/parking_helper.rb
@@ -1,8 +1,6 @@
 module ParkingHelper
   def parking_text_for(onsite, offsite, blue_badge)
     return [t('parking_none')] if onsite == 'parking_onsite_none' && offsite == 'parking_offsite_none' && blue_badge == 'parking_blue_badge_none'
-    return [t(onsite)] if offsite == 'parking_offsite_none'
-    return [t(offsite)] if onsite == 'parking_onsite_none'
     [t(onsite),t(offsite),t(blue_badge)]
   end
 end
