51377652cd4bb781bdab6955473187654552a680
Al Davidson <apdavidson@gmail.com>
2014-11-27 10:05:19 +0000

    100% coverage on admin_facilities_controller and admin_opening_types_controller

--- spec/controllers/admin_facilities_controller_spec.rb
+++ spec/controllers/admin_facilities_controller_spec.rb
@@ -1,25 +1,240 @@
 require 'spec_helper'
 
 describe Admin::FacilitiesController do
-  render_views
-
   before :each do
     sign_in User.create!(name: 'hello', admin: true, email: 'lol@biz.info', password: 'irrelevant')
   end
 
-  it "purges the cache when an object is updated" do
-    object = Facility.create!
-    controller.should_receive(:purge_all_pages)
-    post :update, id: object.id, facility: {}
-    response.should redirect_to(admin_facility_path(object))
+  describe "#update" do
+    let(:facility){ Facility.new(id: 123) }
+    before{ 
+      Facility.stub(:find).and_return(facility) 
+      facility.stub(id: 123)
+    }
+
+    let(:params){ { id: 123, facility: {name: 'new contact type'} } }
+
+    context "that works" do
+      before{ 
+        Facility.any_instance.stub(update_attributes: true)
+      }
+
+      it "purges the cache" do
+        controller.should_receive(:purge_all_pages)
+        post :update, params
+      end
+
+      it "redirects to the show path" do
+        post :update, params
+        response.should redirect_to(admin_facility_path(facility))
+      end
+    
+      it "responds to html" do
+        post :update, params.merge(format: :html)
+        expect(response.content_type).to eq('text/html')
+      end
+
+      it "responds to json" do
+        post :update, params.merge(format: :json)
+        expect(response.content_type).to eq('application/json')
+      end
+    end
+
+    context "that doesn't work" do
+      before{ 
+        Facility.any_instance.stub(update_attributes: false)
+      }
+
+      it "does not purge the cache" do
+        controller.should_not_receive(:purge_all_pages)
+        post :update, params
+      end
+
+      context "a html request" do
+        before{ params[:format] = :html }
+  
+        it "rerenders the edit path" do
+          post :update, params
+          response.should render_template(:edit)
+        end
+    
+        it "responds with html" do
+          post :update, params.merge(format: :html)
+          expect(response.content_type).to eq('text/html')
+        end
+      end
+
+      context "a json request" do
+        before{ params[:format] = :json }
+        it "responds to json" do
+          post :update, params.merge(format: :json)
+          expect(response.content_type).to eq('application/json')
+        end
+      end
+    end
+
   end
 
-  it "purges the cache when a new object is created" do
+  describe "#create" do
+    let(:params){ { facility: {name: 'new contact type'} } }
+
+    context "that saves ok" do
+      it "creates an contact type" do
+        expect{ 
+          post :create, params
+        }.to change { Facility.count }.by(1)
+      end
+
+      it "purges the cache" do
+        controller.should_receive(:purge_all_pages)
+        post :create, params
+      end
+
+      it "redirects to the show path" do
+        post :create, params
+        response.should redirect_to(admin_facility_path(assigns(:facility)))
+      end
+
+      it "responds to html" do
+        post :create, params.merge(format: :html)
+        expect(response.content_type).to eq('text/html')
+      end
+
+      it "responds to json" do
+        post :create, params.merge(format: :json)
+        expect(response.content_type).to eq('application/json')
+      end
+    end
+    context "that doesn't save ok" do
+      before{ Facility.any_instance.stub(save: false) }
+      
+      it "does not create an contact type" do
+        expect{ 
+          post :create, params
+        }.to_not change { Facility.count }
+      end
+
+      it "does not purge the cache" do
+        controller.should_not_receive(:purge_all_pages)
+        post :create, params
+      end
+
+      it "rerenders the new template" do
+        post :create, params
+        response.should render_template(:new)
+      end
+
+      it "responds to html" do
+        post :create, params.merge(format: :html)
+        expect(response.content_type).to eq('text/html')
+      end
+
+      describe "a json request" do
+        it "responds with json" do
+          post :create, params.merge(format: :json)
+          expect(response.content_type).to eq('application/json')
+        end
+
+        it "returns unprocessable_entity status" do
+          post :create, params.merge(format: :json)
+          expect(response.status).to eq(422)
+        end
+      end
+    end
+  end
+
+
+  it "purges the cache when a facility is destroyed" do
+    at = Facility.create!
     expect {
       controller.should_receive(:purge_all_pages)
-      post :create, facility: {}
-      response.should redirect_to(admin_facility_path(assigns(:facility)))
-    }.to change { Facility.count }.by(1)
+      post :destroy, id: at.id
+      response.should redirect_to(admin_facilities_path)
+    }.to change { Facility.count }.by(-1)
+  end
+
+  describe "#index" do
+    it "assigns all facilities to @facilities" do
+      get :index
+      expect(assigns[:facilities]).to eq(Facility.all)
+    end
+
+    it "responds to html" do
+      get :index, format: :html
+      expect(response.content_type).to eq('text/html')
+    end
+
+    it "responds to json" do
+      get :index, format: :json
+      expect(response.content_type).to eq('application/json')
+    end
+  end
+
+  describe "#show" do
+    let(:mock_facility){ Facility.new(id: 123, name: 'mock contact type') }
+    before{ 
+      Facility.stub(:find).and_return(mock_facility)
+    }
+
+    it "gets the right facility" do
+      Facility.should_receive(:find).with('123').and_return(mock_facility)
+      get :show, id: 123
+    end
+
+    it "assigns the facility" do
+      get :show, id: 123
+      expect(assigns[:facility]).to eq(mock_facility)
+    end
+
+    it "responds to html" do
+      get :show, id: 123, format: :html
+      expect(response.content_type).to eq('text/html')
+    end
+
+    it "responds to json" do
+      get :show, id: 123, format: :json
+      expect(response.content_type).to eq('application/json')
+    end
+  end
+
+  describe "#new" do
+
+    it "assigns a new facility" do
+      get :new
+      expect(assigns[:facility]).to be_a(Facility)
+    end
+
+
+    it "responds to html" do
+      get :new, format: :html
+      expect(response.content_type).to eq('text/html')
+    end
+
+    it "responds to json" do
+      get :new, format: :json
+      expect(response.content_type).to eq('application/json')
+    end
+  end
+
+  describe "#edit" do
+    let(:mock_facility){ Facility.new(id: 123, name: 'mock contact type') }
+    before{ 
+      Facility.stub(:find).and_return(mock_facility)
+    }
+
+    it "gets the right facility" do
+      Facility.should_receive(:find).with('123').and_return(mock_facility)
+      get :edit, id: 123
+    end
+  end
+
+  it "purges the cache when a contact type is destroyed" do
+    at = Facility.create!
+    expect {
+      controller.should_receive(:purge_all_pages)
+      post :destroy, id: at.id
+      response.should redirect_to(admin_facilities_path)
+    }.to change { Facility.count }.by(-1)
   end
 
   it "purges the cache when an object is destroyed" do
@@ -30,4 +245,5 @@ describe Admin::FacilitiesController do
       response.should redirect_to(admin_facilities_path)
     }.to change { Facility.count }.by(-1)
   end
+
 end
--- spec/controllers/admin_opening_types_controller_spec.rb
+++ spec/controllers/admin_opening_types_controller_spec.rb
@@ -1,25 +1,241 @@
 require 'spec_helper'
 
 describe Admin::OpeningTypesController do
-  render_views
 
   before :each do
     sign_in User.create!(name: 'hello', admin: true, email: 'lol@biz.info', password: 'irrelevant')
   end
 
-  it "purges the cache when an object is updated" do
-    object = OpeningType.create!
-    controller.should_receive(:purge_all_pages)
-    post :update, id: object.id, opening_type: {}
-    response.should redirect_to(admin_opening_type_path(object))
+  describe "#update" do
+    let(:opening_type){ OpeningType.new(id: 123) }
+    before{ 
+      OpeningType.stub(:find).and_return(opening_type) 
+      opening_type.stub(id: 123)
+    }
+
+    let(:params){ { id: 123, opening_type: {name: 'new opening type'} } }
+
+    context "that works" do
+      before{ 
+        OpeningType.any_instance.stub(update_attributes: true)
+      }
+
+      it "purges the cache" do
+        controller.should_receive(:purge_all_pages)
+        post :update, params
+      end
+
+      it "redirects to the show path" do
+        post :update, params
+        response.should redirect_to(admin_opening_type_path(opening_type))
+      end
+    
+      it "responds to html" do
+        post :update, params.merge(format: :html)
+        expect(response.content_type).to eq('text/html')
+      end
+
+      it "responds to json" do
+        post :update, params.merge(format: :json)
+        expect(response.content_type).to eq('application/json')
+      end
+    end
+
+    context "that doesn't work" do
+      before{ 
+        OpeningType.any_instance.stub(update_attributes: false)
+      }
+
+      it "does not purge the cache" do
+        controller.should_not_receive(:purge_all_pages)
+        post :update, params
+      end
+
+      context "a html request" do
+        before{ params[:format] = :html }
+  
+        it "rerenders the edit path" do
+          post :update, params
+          response.should render_template(:edit)
+        end
+    
+        it "responds with html" do
+          post :update, params.merge(format: :html)
+          expect(response.content_type).to eq('text/html')
+        end
+      end
+
+      context "a json request" do
+        before{ params[:format] = :json }
+        it "responds to json" do
+          post :update, params.merge(format: :json)
+          expect(response.content_type).to eq('application/json')
+        end
+      end
+    end
+
   end
 
-  it "purges the cache when a new object is created" do
+  describe "#create" do
+    let(:params){ { opening_type: {name: 'new opening type'} } }
+
+    context "that saves ok" do
+      it "creates an opening type" do
+        expect{ 
+          post :create, params
+        }.to change { OpeningType.count }.by(1)
+      end
+
+      it "purges the cache" do
+        controller.should_receive(:purge_all_pages)
+        post :create, params
+      end
+
+      it "redirects to the show path" do
+        post :create, params
+        response.should redirect_to(admin_opening_type_path(assigns(:opening_type)))
+      end
+
+      it "responds to html" do
+        post :create, params.merge(format: :html)
+        expect(response.content_type).to eq('text/html')
+      end
+
+      it "responds to json" do
+        post :create, params.merge(format: :json)
+        expect(response.content_type).to eq('application/json')
+      end
+    end
+    context "that doesn't save ok" do
+      before{ OpeningType.any_instance.stub(save: false) }
+      
+      it "does not create an opening type" do
+        expect{ 
+          post :create, params
+        }.to_not change { OpeningType.count }
+      end
+
+      it "does not purge the cache" do
+        controller.should_not_receive(:purge_all_pages)
+        post :create, params
+      end
+
+      it "rerenders the new template" do
+        post :create, params
+        response.should render_template(:new)
+      end
+
+      it "responds to html" do
+        post :create, params.merge(format: :html)
+        expect(response.content_type).to eq('text/html')
+      end
+
+      describe "a json request" do
+        it "responds with json" do
+          post :create, params.merge(format: :json)
+          expect(response.content_type).to eq('application/json')
+        end
+
+        it "returns unprocessable_entity status" do
+          post :create, params.merge(format: :json)
+          expect(response.status).to eq(422)
+        end
+      end
+    end
+  end
+
+
+  it "purges the cache when a opening_type is destroyed" do
+    at = OpeningType.create!
     expect {
       controller.should_receive(:purge_all_pages)
-      post :create, opening_type: {}
-      response.should redirect_to(admin_opening_type_path(assigns(:opening_type)))
-    }.to change { OpeningType.count }.by(1)
+      post :destroy, id: at.id
+      response.should redirect_to(admin_opening_types_path)
+    }.to change { OpeningType.count }.by(-1)
+  end
+
+  describe "#index" do
+    it "assigns all opening_types to @opening_types" do
+      get :index
+      expect(assigns[:opening_types]).to eq(OpeningType.all)
+    end
+
+    it "responds to html" do
+      get :index, format: :html
+      expect(response.content_type).to eq('text/html')
+    end
+
+    it "responds to json" do
+      get :index, format: :json
+      expect(response.content_type).to eq('application/json')
+    end
+  end
+
+  describe "#show" do
+    let(:mock_opening_type){ OpeningType.new(id: 123, name: 'mock opening type') }
+    before{ 
+      OpeningType.stub(:find).and_return(mock_opening_type)
+    }
+
+    it "gets the right opening_type" do
+      OpeningType.should_receive(:find).with('123').and_return(mock_opening_type)
+      get :show, id: 123
+    end
+
+    it "assigns the opening_type" do
+      get :show, id: 123
+      expect(assigns[:opening_type]).to eq(mock_opening_type)
+    end
+
+    it "responds to html" do
+      get :show, id: 123, format: :html
+      expect(response.content_type).to eq('text/html')
+    end
+
+    it "responds to json" do
+      get :show, id: 123, format: :json
+      expect(response.content_type).to eq('application/json')
+    end
+  end
+
+  describe "#new" do
+
+    it "assigns a new opening_type" do
+      get :new
+      expect(assigns[:opening_type]).to be_a(OpeningType)
+    end
+
+
+    it "responds to html" do
+      get :new, format: :html
+      expect(response.content_type).to eq('text/html')
+    end
+
+    it "responds to json" do
+      get :new, format: :json
+      expect(response.content_type).to eq('application/json')
+    end
+  end
+
+  describe "#edit" do
+    let(:mock_opening_type){ OpeningType.new(id: 123, name: 'mock opening type') }
+    before{ 
+      OpeningType.stub(:find).and_return(mock_opening_type)
+    }
+
+    it "gets the right opening_type" do
+      OpeningType.should_receive(:find).with('123').and_return(mock_opening_type)
+      get :edit, id: 123
+    end
+  end
+
+  it "purges the cache when a opening type is destroyed" do
+    at = OpeningType.create!
+    expect {
+      controller.should_receive(:purge_all_pages)
+      post :destroy, id: at.id
+      response.should redirect_to(admin_opening_types_path)
+    }.to change { OpeningType.count }.by(-1)
   end
 
   it "purges the cache when an object is destroyed" do
@@ -30,4 +246,5 @@ describe Admin::OpeningTypesController do
       response.should redirect_to(admin_opening_types_path)
     }.to change { OpeningType.count }.by(-1)
   end
+
 end
