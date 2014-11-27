7fba79be34ce967baff50e9f4ac1969b7ffbb273
Al Davidson <apdavidson@gmail.com>
2014-11-27 09:44:30 +0000

    100% coverage on admin_countries_controller.rb

--- spec/controllers/admin_countries_controller_spec.rb
+++ spec/controllers/admin_countries_controller_spec.rb
@@ -1,25 +1,240 @@
 require 'spec_helper'
 
 describe Admin::CountriesController do
-  render_views
 
   before :each do
     sign_in User.create!(name: 'hello', admin: true, email: 'lol@biz.info', password: 'irrelevant')
   end
 
-  it "purges the cache when an object is updated" do
-    object = Country.create!
-    controller.should_receive(:purge_all_pages)
-    post :update, id: object.id, country: {}
-    response.should redirect_to(edit_admin_country_path(object))
+  describe "#update" do
+    let(:country){ Country.new(id: 123) }
+    before{ 
+      Country.stub(:find).and_return(country) 
+      country.stub(id: 123)
+    }
+
+    let(:params){ { id: 123, country: {name: 'new contact type'} } }
+
+    context "that works" do
+      before{ 
+        Country.any_instance.stub(update_attributes: true)
+      }
+
+      it "purges the cache" do
+        controller.should_receive(:purge_all_pages)
+        post :update, params
+      end
+
+      it "redirects to the edit path" do
+        post :update, params
+        response.should redirect_to(edit_admin_country_path(country))
+      end
+    
+      it "responds to html" do
+        post :update, params.merge(format: :html)
+        expect(response.content_type).to eq('text/html')
+      end
+
+      it "responds to json" do
+        post :update, params.merge(format: :json)
+        expect(response.content_type).to eq('application/json')
+      end
+    end
+
+    context "that doesn't work" do
+      before{ 
+        Country.any_instance.stub(update_attributes: false)
+      }
+
+      it "does not purge the cache" do
+        controller.should_not_receive(:purge_all_pages)
+        post :update, params
+      end
+
+      context "a html request" do
+        before{ params[:format] = :html }
+  
+        it "rerenders the edit path" do
+          post :update, params
+          response.should render_template(:edit)
+        end
+    
+        it "responds with html" do
+          post :update, params.merge(format: :html)
+          expect(response.content_type).to eq('text/html')
+        end
+      end
+
+      context "a json request" do
+        before{ params[:format] = :json }
+        it "responds to json" do
+          post :update, params.merge(format: :json)
+          expect(response.content_type).to eq('application/json')
+        end
+      end
+    end
+
   end
 
-  it "purges the cache when a new object is created" do
+  describe "#create" do
+    let(:params){ { country: {name: 'new contact type'} } }
+
+    context "that saves ok" do
+      it "creates an contact type" do
+        expect{ 
+          post :create, params
+        }.to change { Country.count }.by(1)
+      end
+
+      it "purges the cache" do
+        controller.should_receive(:purge_all_pages)
+        post :create, params
+      end
+
+      it "redirects to the edit path" do
+        post :create, params
+        response.should redirect_to(edit_admin_country_path(assigns(:country)))
+      end
+
+      it "responds to html" do
+        post :create, params.merge(format: :html)
+        expect(response.content_type).to eq('text/html')
+      end
+
+      it "responds to json" do
+        post :create, params.merge(format: :json)
+        expect(response.content_type).to eq('application/json')
+      end
+    end
+    context "that doesn't save ok" do
+      before{ Country.any_instance.stub(save: false) }
+      
+      it "does not create an contact type" do
+        expect{ 
+          post :create, params
+        }.to_not change { Country.count }
+      end
+
+      it "does not purge the cache" do
+        controller.should_not_receive(:purge_all_pages)
+        post :create, params
+      end
+
+      it "rerenders the new template" do
+        post :create, params
+        response.should render_template(:new)
+      end
+
+      it "responds to html" do
+        post :create, params.merge(format: :html)
+        expect(response.content_type).to eq('text/html')
+      end
+
+      describe "a json request" do
+        it "responds with json" do
+          post :create, params.merge(format: :json)
+          expect(response.content_type).to eq('application/json')
+        end
+
+        it "returns unprocessable_entity status" do
+          post :create, params.merge(format: :json)
+          expect(response.status).to eq(422)
+        end
+      end
+    end
+  end
+
+  it "purges the cache when a contact type is destroyed" do
+    at = Country.create!
     expect {
       controller.should_receive(:purge_all_pages)
-      post :create, country: {}
-      response.should redirect_to(edit_admin_country_path(assigns(:country)))
-    }.to change { Country.count }.by(1)
+      post :destroy, id: at.id
+      response.should redirect_to(admin_countries_path)
+    }.to change { Country.count }.by(-1)
+  end
+
+  describe "#index" do
+    it "assigns all countries to @countries" do
+      get :index
+      expect(assigns[:countries]).to eq(Country.all)
+    end
+
+    it "responds to html" do
+      get :index, format: :html
+      expect(response.content_type).to eq('text/html')
+    end
+
+    it "responds to json" do
+      get :index, format: :json
+      expect(response.content_type).to eq('application/json')
+    end
+  end
+
+  describe "#show" do
+    let(:mock_country){ Country.new(id: 123, name: 'mock contact type') }
+    before{ 
+      Country.stub(:find).and_return(mock_country)
+    }
+
+    it "gets the right country" do
+      Country.should_receive(:find).with('123').and_return(mock_country)
+      get :show, id: 123
+    end
+
+    it "assigns the country" do
+      get :show, id: 123
+      expect(assigns[:country]).to eq(mock_country)
+    end
+
+    it "responds to html" do
+      get :show, id: 123, format: :html
+      expect(response.content_type).to eq('text/html')
+    end
+
+    it "responds to json" do
+      get :show, id: 123, format: :json
+      expect(response.content_type).to eq('application/json')
+    end
+  end
+
+  describe "#new" do
+
+    it "assigns a new country" do
+      get :new
+      expect(assigns[:country]).to be_a(Country)
+    end
+
+
+    it "responds to html" do
+      get :new, format: :html
+      expect(response.content_type).to eq('text/html')
+    end
+
+    it "responds to json" do
+      get :new, format: :json
+      expect(response.content_type).to eq('application/json')
+    end
+  end
+
+  describe "#edit" do
+    let(:mock_country){ Country.new(id: 123, name: 'mock contact type') }
+    before{ 
+      Country.stub(:find).and_return(mock_country)
+    }
+
+    it "gets the right country" do
+      Country.should_receive(:find).with('123').and_return(mock_country)
+      get :edit, id: 123
+    end
+  end
+
+  it "purges the cache when a contact type is destroyed" do
+    at = Country.create!
+    expect {
+      controller.should_receive(:purge_all_pages)
+      post :destroy, id: at.id
+      response.should redirect_to(admin_countries_path)
+    }.to change { Country.count }.by(-1)
   end
 
   it "purges the cache when an object is destroyed" do
