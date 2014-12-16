e001ed81a05cfb412ffb3f4a1254105f60d91f24
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2014-12-16 15:16:58 +0000

    remove details_updated_at

--- db/migrate/20141215180524_add_gov_uk_fields_to_court.rb
+++ db/migrate/20141215180524_add_gov_uk_fields_to_court.rb
@@ -4,7 +4,6 @@ class AddGovUkFieldsToCourt < ActiveRecord::Migration
   def change
     add_column :courts, :uuid, :string
     add_column :courts, :gov_uk_md5, :string
-    add_column :courts, :details_changed_at, :datetime, default: Time.at(0)
     add_column :courts, :gov_uk_updated_at, :datetime, default: Time.at(0)
     add_index :courts, :uuid, unique: true
   end
