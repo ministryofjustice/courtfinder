16ab08a35ee4218df1ce68094bd1aceb8f5f6dc3
Al Davidson <apdavidson@gmail.com>
2014-12-22 16:10:11 +0000

    switched court image upload storage from fog to carrierwave-aws after getting bizarre Excon 403 errors from s3

--- app/uploaders/court_images_uploader.rb
+++ app/uploaders/court_images_uploader.rb
@@ -11,7 +11,7 @@ class CourtImagesUploader < CarrierWave::Uploader::Base
   # include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
-  storage :fog
+  #storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
