2bec741e9343d53bd2b0e9d0feb1d3655df8e272
Lukasz Raczylo <lukasz@raczylo.com>
2015-09-09 12:26:18 +0100

    Added missing content manually. This one should be removed when we'll figure out where does it come from.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,22 +1,99 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
-//= require jquery_ujs
-//= require jquery-migrate
-//= require gmaps4rails/gmaps4rails.base
-//= require gmaps4rails/gmaps4rails.googlemaps
-//
-//= require moj
-//= require modules/moj.cookie-message
-//= require modules/moj.tabs
-//= require search
-//= require main
+(function ($) {
+  'use strict';
+    $(function () {
+        if (!supportsInputAttribute('autofocus')) {
+            $('[autofocus]').focus();
+        }
+    });
+    // detect support for input attirbute
+    function supportsInputAttribute (attr) {
+        var input = document.createElement('input');
+        return attr in input;
+    }
+
+
+  // Form focus styles
+  // from https://github.com/alphagov/govuk_elements/blob/master/public/javascripts/application.js
+
+  $('.block-label').each(function() {
+
+    // Add focus
+    $('.block-label input').focus(function() {
+      $('label[for="' + this.id + '"]').addClass('add-focus');
+      }).blur(function() {
+      $('label').removeClass('add-focus');
+    });
+  });
+
+})(jQuery);
+
+(function ($) {
+  'use strict';
+
+  if( $('#search-index-page').length > 0 ){
+
+    // "Next" Button on /search/
+    $('#search-index-page #start-button').click(function (){
+        ga('send', 'event', 'search-index-page', 'next-button', 'Next button clicked to start');
+    });
+
+    // "Name or address" link on /search/
+    $('#search-index-page a[href="/search/address"]').click(function (){
+        ga('send', 'event', 'search-index-page', 'name-or-address-link', 'Name or address link clicked on start page');
+    });
+
+    // "A-Z list of all courts" link on /search/
+    $('#search-index-page a[href="/courts/"]').click(function (){
+        ga('send', 'event', 'search-index-page', 'a-z-courts-link', 'A-Z list of all courts link clicked on start page');
+    });
+  }
+
+  if( $('#aol-page').length > 0 ){
+    $('#aol-page form[action^="/search/"]').submit(function (){
+      var val = $('#aol-page label input:checked').val();
+      var name = val.toLowerCase().replace(' ', '-');
+
+      ga('send', 'event', 'aol-page', 'aol-' + name + '-selected', 'Area of Law selected: ' + val);
+    });
+  }
+
+  if( $('#spoe-page').length > 0 ){
+    $('#spoe-page form[action^="/search"]').submit(function (){
+      var selected = $('#spoe-page form label input:checked').val();
+      var label = selected == 'start' ? "Starting a new claim selected" : "Already in contact selected";
+
+      ga('send', 'event', 'spoe-page', 'spoe-' + selected + 'selected', label);
+    });
+  }
+
+  if( $('#postcode-page').length > 0 ){
+    $('#postcode-page #housing-possession-online-link').click(function (){
+      ga('send', 'event', 'postcode-page', 'housing-possession-online-link-clicked', 'Housing possession online link clicked');
+    });
+
+    $('#postcode-page #money-claims-online-link').click(function (){
+      ga('send', 'event', 'postcode-page', 'money-claims-online-link-clicked', 'Money claims online link clicked');
+    });
+  }
+
+  if( $('#search-results-page').length > 0 ){
+    $(function (){
+      ga('send', 'event', 'search-results-page', 'looking-at-search-results', 'User is looking at search results');
+    });
+
+    $('#search-results-page .court-map a').click(function (){
+      ga('send', 'event', 'search-results-page', 'map-link-clicked', 'A map link clicked on search results');
+    });
+
+    $('#search-results-page .more-details-link').click(function (){
+      ga('send', 'event', 'search-results-page', 'more-details-link-clicked', 'A more details link clicked on search results');
+    });
+  }
+
+  if( $('#court-detail-page').length > 0 ){
+    $(function (){
+      ga('send', 'event', 'court-detail-page', 'looking-at-court-page', 'User is looking at a court page');
+    });
+  }
+
+})(jQuery);
