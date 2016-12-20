d892751a47d91203b8d0c2198b3049bd02443666
Dean <dean.bullock@cgi.com>
2016-12-20 09:07:04 +0000

    Implemented A-Z Court Filter on Areas of Law table

--- app/assets/javascripts/admin/fixedheader.js
+++ app/assets/javascripts/admin/fixedheader.js
@@ -1,31 +1,48 @@
 $(document).ready(function(){
-    var table = $('.areaOfLaw');
-
-    table.DataTable(
-    	{
-    		fixedColumns: {
-        		leftColumns: 2
-    		},
-    		
-        	scrollY:        300,
-        	scrollX:        true,
-        	paging: 		true,
-        	sorting:        false,
-        	bSmart: false,
-            columnDefs: [{ targets: '_all', "searchable": false }, 
-                        { targets: [2], "searchable": true}]
-       	}); //.fnFilter( '^'+this.value, 2, true, false );
-
-
-    // table.columns().eq( 0 ).each( function ( colIdx ) {
-    //     if (colIdx != 2 || rowIdx < 1)
-    //         return; //Do not add event handlers for these columns
-
-    //     $( 'td', table.column( colIdx ).footer() ).on( 'keyup change', function () {
-    //         table
-    //             .column( colIdx )
-    //             .search( this.value )
-    //             .draw();
-    //     } );
-    // } );
+    var table = $('.areaOfLaw').DataTable(
+        {
+            fixedColumns: {
+                leftColumns: 2
+            },
+            
+            scrollY:        300,
+            scrollX:        true,
+            paging:         true,
+            sorting:        false
+        });
+    $('ul.tabs-nav.clearfix > li > a').click( function() {
+        var letter = $(this).text();
+        console.log(letter);
+        if ( _alphabetSearch === letter)
+        {
+            _alphabetSearch = '';
+            $('ul.tabs-nav.clearfix').find( 'a.active' ).removeClass( 'active' );
+        }
+        else
+        {
+            _alphabetSearch = letter;
+            $('ul.tabs-nav.clearfix').find( 'a.active' ).removeClass( 'active' );
+            $(this).addClass( 'active' );
+        }
+        table.draw();
+       
+    } );
+  
 });
+
+
+
+var _alphabetSearch = '';
+ 
+$.fn.dataTable.ext.search.push( function ( settings, searchData ) {
+    if ( ! _alphabetSearch ) {
+        return true;
+    }
+ 
+    if ( searchData[1].charAt(0) === _alphabetSearch ) {
+        return true;
+    }
+ 
+    return false;
+} );
+ 
