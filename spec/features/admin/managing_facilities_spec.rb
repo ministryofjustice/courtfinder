493d8b03b4374d74ba777ced72393b61d4b98056
Petr Zaparka <petr@zaparka.cz>
2017-02-28 15:03:00 +0000

    Adding test for image dimension validation.

--- spec/features/admin/managing_facilities_spec.rb
+++ spec/features/admin/managing_facilities_spec.rb
@@ -12,12 +12,11 @@ feature 'As an admin I should be able to manage facility' do
     visit new_admin_facility_path
 
     expect(page).to have_content('New facility_type')
-    fill_in('Name', with: 'Baby')
-    fill_in('Image description', with: 'Baby change')
     expect(page).not_to have_xpath('.//input[@id="facility_image"]')
 
-    image_path = "#{ Rails.root }/spec/fixtures/assets/firstaid.png"
-    attach_file('facility_image_file', image_path)
+    fill_in('Name', with: 'Baby')
+    fill_in('Image description', with: 'Baby change')
+    upload_icon('firstaid.png')
     click_button "Create Facility"
 
     expect(page).to have_content('Facility type was successfully created.')
@@ -27,25 +26,33 @@ feature 'As an admin I should be able to manage facility' do
     expect(page).to have_xpath('.//img[@title="Baby change"]')
   end
 
-  it 'I am able to update facility' do
-    file = File.open("#{ Rails.root }/spec/fixtures/assets/firstaid.png")
-    facility = create(:facility, name: 'baby', image_file: file)
+  context 'existing facility' do
+    before do
+      file = File.open("#{ Rails.root }/spec/fixtures/assets/firstaid.png")
+      facility = create(:facility, name: 'baby', image_file: file)
+      visit edit_admin_facility_path(facility)
+    end
 
-    visit edit_admin_facility_path(facility)
+    it 'I am able to update facility' do
+      expect(page).to have_content('Editing facility')
+      expect(page).not_to have_xpath('.//input[@id="facility_image"]')
 
-    expect(page).to have_content('Editing facility')
-    fill_in('Name', with: 'Hotspot')
-    fill_in('Image description', with: 'Wifi is available')
-    expect(page).not_to have_xpath('.//input[@id="facility_image"]')
+      fill_in('Name', with: 'Hotspot')
+      fill_in('Image description', with: 'Wifi is available')
+      upload_icon('hotspot.png')
+      click_button "Update Facility"
 
-    attach_file('facility_image_file', "#{ Rails.root }/spec/fixtures/assets/hotspot.png")
-    click_button "Update Facility"
+      expect(page).to have_content('Facility type was successfully updated.')
 
-    expect(page).to have_content('Facility type was successfully updated.')
+      expect(page).to have_content('Name: Hotspot')
+      expect(page).to have_content('Image description: Wifi is available')
+      expect(page).to have_xpath('.//img[@title="Wifi is available"]')
+      expect(page.find(:xpath, './/img[@title="Wifi is available"]')['src']).to include('hotspot.png')
+    end
+  end
 
-    expect(page).to have_content('Name: Hotspot')
-    expect(page).to have_content('Image description: Wifi is available')
-    expect(page).to have_xpath('.//img[@title="Wifi is available"]')
-    expect(page.find(:xpath, './/img[@title="Wifi is available"]')['src']).to include('hotspot.png')
+  def upload_icon(name)
+    image_path = "#{ Rails.root }/spec/fixtures/assets/#{name}"
+    attach_file('facility_image_file', image_path)
   end
 end
