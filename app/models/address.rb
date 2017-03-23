4b1ef7604b8940a13374588a81fef463a6a66d78
Petr Zaparka <petr@zaparka.cz>
2017-03-23 11:51:00 +0000

    Updating rubocop config.

--- app/models/address.rb
+++ app/models/address.rb
@@ -22,13 +22,14 @@ class Address < ActiveRecord::Base
   belongs_to :town
   belongs_to :address_type
   belongs_to :court
-  attr_accessible :address_line_1, :address_line_2, :address_line_3, :address_line_4, :dx, :name, :postcode, :town_id, :address_type_id, :is_primary, :town
+  attr_accessible :address_line_1, :address_line_2, :address_line_3,
+    :address_line_4, :dx, :name, :postcode, :town_id, :address_type_id, :is_primary, :town
   validates_presence_of :address_line_1, :town
 
-  has_paper_trail ignore: [:created_at, :updated_at], meta: {ip: :ip}
+  has_paper_trail ignore: [:created_at, :updated_at], meta: { ip: :ip }
 
-  scope :visiting, ->() { where(:address_type_id => AddressType.find_by_name("Visiting").try(:id)) }
-  scope :postal, ->() { where(:address_type_id => AddressType.find_by_name("Postal").try(:id)) }
+  scope :visiting, ->() { where(address_type_id: AddressType.find_by_name("Visiting").try(:id)) }
+  scope :postal, ->() { where(address_type_id: AddressType.find_by_name("Postal").try(:id)) }
 
   def lines
     [
@@ -40,12 +41,12 @@ class Address < ActiveRecord::Base
   end
 
   # Output address_lines fields of an address separated by comma or specified delimiter
-  def address_lines(glue=',')
+  def address_lines(glue = ',')
     lines.join glue
   end
 
   # Output full address separated by comma or specified delimiter
-  def full(glue=',')
+  def full(glue = ',')
     full_address = lines
     full_address += [town.name, town.county.name] if town.present?
     full_address.push postcode
