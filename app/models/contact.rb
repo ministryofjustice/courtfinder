192147f2c740779612c973e4bb0844ac56d08e1a
Petr Zaparka <petr@zaparka.cz>
2017-03-24 09:35:23 +0000

    First batch of formating updates for rubobocop.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -18,17 +18,17 @@ class Contact < ActiveRecord::Base
   alias_attribute :number_explanation, :explanation
   attr_accessible :in_leaflet, :name, :sort, :telephone, :contact_type_id, :explanation, :number_explanation
 
-  validates :telephone, presence:true, contact: true
-  validate :explanation_length
+  validates :telephone, presence: true, contact: true
+  validates_length_of :explanation, maximum: 85
 
-  has_paper_trail ignore: [:created_at, :updated_at], meta: {ip: :ip}
+  has_paper_trail ignore: [:created_at, :updated_at], meta: { ip: :ip }
 
   default_scope { order(:sort) }
   scope :with_telephones, -> { where("contact_type_id != ?", ContactType.find_by_name('DX')) }
   scope :with_dx_numbers, -> { where("contact_type_id = ?", ContactType.find_by_name('DX')) }
 
   def contact_type_name
-    self.contact_type.name.blank? ? "another service" : self.contact_type.name
+    contact_type.name.blank? ? "another service" : contact_type.name
   end
 
   private
--- app/models/court.rb
+++ app/models/court.rb
@@ -51,11 +51,12 @@ class Court < ActiveRecord::Base
   has_many :postcode_courts, dependent: :destroy
 
   attr_accessible :court_number, :info, :name, :slug, :area_id, :cci_code, :old_id,
-                  :old_court_type_id, :area, :addresses_attributes, :latitude, :longitude, :court_type_ids,
-                  :address_ids, :area_of_law_ids, :opening_times_attributes, :contacts_attributes, :emails_attributes,
-                  :court_facilities_attributes, :image, :image_file, :remove_image_file, :display, :alert,
-                  :info_leaflet, :defence_leaflet, :prosecution_leaflet, :juror_leaflet,
-                  :postcode_list, :directions, :parking_onsite, :parking_offsite, :parking_blue_badge, :hide_aols
+    :old_court_type_id, :area, :addresses_attributes, :latitude, :longitude, :court_type_ids,
+    :address_ids, :area_of_law_ids, :opening_times_attributes, :contacts_attributes,
+    :emails_attributes, :court_facilities_attributes, :image, :image_file,
+    :remove_image_file, :display, :alert, :info_leaflet, :defence_leaflet,
+    :prosecution_leaflet, :juror_leaflet, :postcode_list, :directions, :parking_onsite,
+    :parking_offsite, :parking_blue_badge, :hide_aols
 
   accepts_nested_attributes_for :addresses, allow_destroy: true
   accepts_nested_attributes_for :opening_times, allow_destroy: true
@@ -67,15 +68,9 @@ class Court < ActiveRecord::Base
   before_validation :convert_visiting_to_location
 
   validates :name, presence: true
-
-
-  # lattitude and longitude validations temporarily disabled to allow mass update of UUIDs
-  # validates :latitude, numericality: { greater_than:  -90, less_than:  90 }, presence: true, if: :has_visiting_address?
-  # validates :longitude, numericality: { greater_than: -180, less_than: 180 }, presence: true, if: :has_visiting_address?
-
   validate :check_postcode_errors
 
-  has_paper_trail ignore: [:created_at, :updated_at], meta: {ip: :ip}
+  has_paper_trail ignore: [:created_at, :updated_at], meta: { ip: :ip }
 
   extend FriendlyId
   friendly_id :name, use: [:slugged, :history, :finders]
@@ -84,30 +79,42 @@ class Court < ActiveRecord::Base
 
   mount_uploader :image_file, CourtImagesUploader
 
-  acts_as_gmappable :process_geocoding => lambda { |obj| obj.addresses.first.address_line_1.present? },
-                    :validation => false,
-                    :process_geocoding => false
+  acts_as_gmappable process_geocoding: ->(obj) { obj.addresses.first.address_line_1.present? },
+                    validation: false,
+                    process_geocoding: false
 
   def gmaps4rails_address
-  #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
-    "#{self.addresses.first.address_line_1}, #{self.addresses.first.town.name}, #{self.addresses.first.town.county.name}"
+    # describe how to retrieve the address from your model, if you use directly a db column,
+    # you can dry your code, see wiki
+    "#{addresses.first.address_line_1}, #{addresses.first.town.name},
+     #{addresses.first.town.county.name}"
   end
 
   # Scope methods
   scope :visible,         -> { where(display: true) }
   scope :by_name,         -> { order('courts.name') }
-  scope :by_area_of_law,  -> (area_of_law) { select('courts.*, lower(courts.name)').joins(:areas_of_law).uniq.where(areas_of_law: {name: area_of_law}) if area_of_law.present? }
-  scope :search,          -> (q) { where('courts.name ilike ?', "%#{q.downcase}%") if q.present? }
-  scope :for_local_authority, -> (local_authority) { joins(:local_authorities).where("local_authorities.name" => local_authority) }
-  scope :for_local_authority_and_area_of_law, -> (local_authority, area_of_law) {
-    joins(:local_authorities).where("local_authorities.name" => local_authority, "remits.area_of_law_id" => "#{area_of_law.id}").order(:name)
+  scope :by_area_of_law, lambda { |area_of_law|
+    select('courts.*, lower(courts.name)').
+      joins(:areas_of_law).uniq.
+      where(areas_of_law: { name: area_of_law }) if area_of_law.present?
+  }
+  scope :search, ->(q) { where('courts.name ilike ?', "%#{q.downcase}%") if q.present? }
+  scope :for_local_authority, lambda { |local_authority|
+    joins(:local_authorities).
+      where("local_authorities.name" => local_authority)
+  }
+  scope :for_local_authority_and_area_of_law, lambda { |local_authority, area_of_law|
+    joins(:local_authorities).
+      where("local_authorities.name" => local_authority,
+            "remits.area_of_law_id" => "#{area_of_law.id}").
+      order(:name)
   }
   scope :by_postcode_court_mapping, -> (postcode, area_of_law = nil) {
     if postcode.present?
       if postcode_court = PostcodeCourt.where("court_id IS NOT NULL AND ? like lower(postcode) || '%'",
-            postcode.gsub(/\s+/, "").downcase)
-            .order('-length(postcode)').first
-        #Using a reverse id lookup instead of just postcode_court.court as a workaround for the distance calculator
+            postcode.gsub(/\s+/, "").downcase).
+            order('-length(postcode)').first
+        # Using a reverse id lookup instead of just postcode_court.court as a workaround for the distance calculator
         if area_of_law
           by_area_of_law(area_of_law).where(id: postcode_court.court_id).limit(1)
         else
@@ -125,15 +132,14 @@ class Court < ActiveRecord::Base
     longitude && latitude && !addresses.visiting.empty?
   end
 
-
   def add_uuid
-    self.uuid = UuidGenerator.new.generate if self.uuid.nil?
+    self.uuid = UuidGenerator.new.generate if uuid.nil?
   end
 
   def fetch_image_file
     if image.present?
-      self.image_file.download!("https://hmctscourtfinder.justice.gov.uk/courtfinder/images/courts/#{self.image.to_s}")
-      self.image_file.store!
+      image_file.download!("https://hmctscourtfinder.justice.gov.uk/courtfinder/images/courts/#{image}")
+      image_file.store!
     end
   end
 
@@ -142,7 +148,7 @@ class Court < ActiveRecord::Base
   end
 
   def is_county_court?
-    court_types.any?{|ct| ct.name == 'County Court'}
+    court_types.any? { |ct| ct.name == 'County Court' }
   end
 
   def postcode_list
@@ -158,7 +164,7 @@ class Court < ActiveRecord::Base
         if pc.court && pc.court == self
           new_postcode_courts << pc
         elsif pc.court && pc.court != self
-          @postcode_errors << "Post code \"#{postcode}\" is already assigned to #{pc.court.name}. Please remove it from this court before assigning it to #{self.name}."
+          @postcode_errors << "Post code \"#{postcode}\" is already assigned to #{pc.court.name}. Please remove it from this court before assigning it to #{name}."
         end
       else
         new_postcode_courts << PostcodeCourt.new(postcode: postcode)
@@ -172,7 +178,7 @@ class Court < ActiveRecord::Base
   end
 
   def check_postcode_errors
-    @postcode_errors.each {|e| errors.add(:postcode_courts, e) } if @postcode_errors
+    @postcode_errors.each { |e| errors.add(:postcode_courts, e) } if @postcode_errors
   end
 
   def visiting_addresses
@@ -180,7 +186,7 @@ class Court < ActiveRecord::Base
   end
 
   def has_visiting_address?
-    #Converting to array so that we get the addresses in memory, not the db record, otherwise validations don't work correctly.
+    # Converting to array so that we get the addresses in memory, not the db record, otherwise validations don't work correctly.
     visiting_addresses.count > 0
   end
 
@@ -193,11 +199,12 @@ class Court < ActiveRecord::Base
     self.longitude = nil
     begin
       @cs = CourtSearch.new(visiting_postcode)
-      if lat_lon = @cs.latlng_from_postcode(visiting_postcode)
+      lat_lon = @cs.latlng_from_postcode(visiting_postcode)
+      if lat_lon
         self.latitude = lat_lon[0]
         self.longitude = lat_lon[1]
       end
-    rescue Exception => ex
+    rescue Exception
       Rails.logger.error("Could not get latlng from: visiting_postcode")
     end
   end
@@ -233,18 +240,18 @@ class Court < ActiveRecord::Base
 
   private
 
-    def resolve_leaflets
-      leaflets = ["visitor", "defence", "prosecution", "juror"]
-      court_type = self.court_types.pluck('LOWER(name)')
-      case
-      when court_type.size >= 1 && court_type.include?("crown court")
-        leaflets
-      when court_type.size >= 1 && court_type.include?("magistrates court")
-        leaflets.take(3)
-      when court_type.any?{|ct| ct == "county court" || ct == "family court" || ct == "tribunal" }
-        leaflets.take(1)
-      else
-        leaflets
-      end
+  def resolve_leaflets
+    leaflets = %w(visitor defence prosecution juror)
+    court_type = court_types.pluck('LOWER(name)')
+    case
+    when court_type.size >= 1 && court_type.include?("crown court")
+      leaflets
+    when court_type.size >= 1 && court_type.include?("magistrates court")
+      leaflets.take(3)
+    when court_type.any? { |ct| ct == "county court" || ct == "family court" || ct == "tribunal" }
+      leaflets.take(1)
+    else
+      leaflets
     end
+  end
 end
