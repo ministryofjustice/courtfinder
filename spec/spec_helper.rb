ed5b6f1d30084393372432177a4cac6e83841a55
Leonidas Apostolidis <leoapost@gmail.com>
2017-04-12 16:22:18 +0100

    update gems

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,7 @@
-# must do this before any application code
-require 'simplecov'
-SimpleCov.start
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
+require 'simplecov'
+SimpleCov.start
 
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
@@ -106,4 +102,11 @@ VCR.configure do |config|
   puts "VRC is logging to #{Rails.root}\/log\/vcr.log"
   config.debug_logger = File.open("#{Rails.root}\/log\/vcr.log", 'w')
   config.allow_http_connections_when_no_cassette = false
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
