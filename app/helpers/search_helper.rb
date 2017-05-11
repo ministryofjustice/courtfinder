a58f13e430eed00732f9f54ac7365d4893d79b10
Petr Zaparka <petr@zaparka.cz>
2017-05-11 15:47:53 +0100

    Removing search from home page
    Home page is not used anymore and it's redirected to python app.

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,9 +1,5 @@
 module SearchHelper
 
-  def family_court_search_found(search, area_of_law)
-    "#{'Court'.pluralize(search.results.size)} dealing with applications involving #{area_of_law.name.downcase} for <strong>#{search.query}</strong>".html_safe
-  end
-
   def area_of_law_groups(selected = nil)
     @selected = AreaOfLaw.find(selected) rescue nil
 
