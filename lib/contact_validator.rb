35d0920fb5470ba1719a4a0d597145be81fa8631
Petr Zaparka <petr@zaparka.cz>
2017-05-18 15:28:40 +0100

    Refactoring lib folder.

--- lib/contact_validator.rb
+++ lib/contact_validator.rb
@@ -1,24 +1,30 @@
 class ContactValidator < ActiveModel::EachValidator
+
   def validate_each(record, attribute, value)
     return if is_dx_number?(record)
+    service = check_duplicate_number(record, value)
     if is_valid_phone_format(value) == false
-      record.errors.add(attribute, "is invalid. Please enter one phone number per entry. \"#{value}\" is not a valid phone number. Please enter a valid phone number, only digits and spaces allowed, up to 12 numbers.")
-    elsif service = check_duplicate_number(record, value)
-      record.errors.add(attribute, "is invalid. Phone number #{value} is already entered for #{service}.")
+      message = I18n.t('errors.contact_validator.invalid_phone_format', value: value)
+      record.errors.add(attribute, message)
+    elsif service
+      message = I18n.t('errors.contact_validator.phone_entered', value: value, service: service)
+      record.errors.add(attribute, message)
     end
   end
 
   private
+
   def check_duplicate_number(contact, phone_number)
     court_contacts = contact.court.contacts
-    if i = find_number_in_existing_contacts(court_contacts, phone_number)
-      court_contacts[i].contact_type_name if court_contacts[i] != contact
+    i = find_number_in_existing_contacts(court_contacts, phone_number)
+    if i && court_contacts[i] != contact
+      court_contacts[i].contact_type_name
     end
   end
 
   def find_number_in_existing_contacts(contacts, number)
-    #Strip whitespace of existing contact numbers and check if new number already exists
-    contacts.pluck(:telephone).collect { |t| t.gsub(" ","") }.index(number.gsub(" ",""))
+    # Strip whitespace of existing contact numbers and check if new number already exists
+    contacts.pluck(:telephone).collect { |t| t.delete(" ") }.index(number.delete(" "))
   end
 
   def is_valid_phone_format(phone_number)
--- lib/court_address_validator.rb
+++
@@ -1,8 +0,0 @@
-# class CourtAddressValidator < ActiveModel::Validator
-#   def validate(record)
-#     puts record.addresses.to_yaml
-#     if record.has_visiting_address? && !record.latitude.present?
-#       record.errors[:latitude] <<  "Lat/lon missing."
-#     end
-#   end
-# end
