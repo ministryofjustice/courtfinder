80812406e81c6f10961fbfd9bff96a637faaea4b
Petr Zaparka <petr@zaparka.cz>
2017-08-11 11:58:24 +0100

    Fixing issue with unvalid postcode still present after submit.

--- app/assets/javascripts/admin/tagify.js
+++ app/assets/javascripts/admin/tagify.js
@@ -1,28 +1,34 @@
+
 $('textarea[name="court[postcode_list]"]').tagify({
-  inputValidation: /^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y])))( {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2})?))$/,
   placeholder: 'Add postcode',
   addNewDelimiter: [13],
-  addCb: validate_tag
+  addCb: validateTag,
 })
 
-function validate_tag(tags) {
+$('form.js-postcode-tag-form').on('submit', function(event){
+  if(!confirm('Do you really want to save changes?')){
+    event.preventDefault();
+  }
+
+});
+
+function validateTag(tags) {
   postcode = tags[0].tag
   self = this
 
   $.get('/admin/official_postcodes/validate/'+postcode)
   .done(function(data) {
     if (data.valid == false) {
-      remove_tag($(self))
+      removeTag($(self), postcode)
     }
   })
   .fail(function() {
-    remove_tag($(self))
+    removeTag($(self), postcode)
   })
 
 }
 
-function remove_tag(tag){
-  tag.parent().find('div.tagify-tag').last().remove()
+function removeTag(tag){
+  alert('Postcode that you entered ('+postcode+') is not valid, please try again.')
+  tag.parent().find('i.tagify-remove').last().click()
 }
-
-
